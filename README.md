d234b04d6f21a57f921d82e1ccc1a05eae41bbef
guygreen23 <grguy@post.bgu.ac.il>
2024-01-27 14:09:35 +0200

    Commit 1.5

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Commit 1:
 I have set a basic server, with a folder for backend, that inside that folder we have a folder to write our controllers/functions, and a folder for out routes.
+
+### Commoit 1.5:
+I've added async support.
--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc   Get goals 
 // @route  Get /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 // @desc   Create a goals
 // @route  post /api/goals
 // @access Private
-const setGoals = (req, res) => {
-    res.status(200).json({message: 'Create a goal'})
-}
+const setGoals = asyncHandler(async (req, res) => {
+    if (!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field.')
+    }    
+    res.status(200).json({message: 'Set goal'})
+})
 
 // @desc   Update a goal 
 // @route  put /api/goals/:id
 // @access Private
-const putGoals = (req, res) => {
+const putGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 
 // @desc   Delete a goal
 // @route  delete /api/goals/:id
 // @access Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 
 module.exports = {
